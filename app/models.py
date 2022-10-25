18504f09c8b4201e59ea2dbed61525ac56b8f275
esh8904 <esh8904@gmail.com>
2022-10-25 11:44:19 +0900

    1025_2

---
+++ app/models.py
@@ -0,0 +1,232 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    regist_date = models.DateField(blank=True, null=True)
+    exp_date = models.DateField(blank=True, null=True)
+    notice = models.TextField(blank=True, null=True)
+    price = models.IntegerField(blank=True, null=True)
+    username = models.CharField(max_length=45, blank=True, null=True)    
+    categorie = models.CharField(max_length=45, blank=True, null=True)   
+    being_rented = models.CharField(max_length=45, blank=True, null=True)
+    ident = models.AutoField(primary_key=True)
+    test = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'article'
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    email = models.CharField(max_length=254)
+    username = models.CharField(unique=True, max_length=150)
+    password = models.CharField(max_length=128)
+    date_joined = models.DateTimeField(blank=True, null=True)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    first_name = models.CharField(max_length=150, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
+    is_staff = models.IntegerField()
+    is_active = models.CharField(max_length=10)
+    phone = models.CharField(max_length=45, blank=True, null=True)
+    major = models.CharField(max_length=45, blank=True, null=True)
+    undergrad = models.CharField(max_length=45, blank=True, null=True)
+    name = models.CharField(max_length=45, blank=True, null=True)
+    mileage = models.CharField(max_length=45, blank=True, null=True)
+    adress = models.CharField(max_length=45, blank=True, null=True)
+    state = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class Contact(models.Model):
+    title = models.CharField(primary_key=True, max_length=45)
+    email = models.CharField(max_length=45, blank=True, null=True)
+    content = models.CharField(max_length=45, blank=True, null=True)
+    username = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'contact'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class User(models.Model):
+    email = models.CharField(primary_key=True, max_length=20)
+    password = models.CharField(max_length=300, blank=True, null=True)
+    phone = models.IntegerField(blank=True, null=True)
+    regis_date = models.DateField(blank=True, null=True)
+    borrow_code = models.IntegerField(blank=True, null=True)
+    regist_code = models.IntegerField(blank=True, null=True)
+    major = models.CharField(max_length=45, blank=True, null=True)
+    undergrad = models.CharField(max_length=45, blank=True, null=True)
+    username = models.CharField(max_length=45, blank=True, null=True)
+    name = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'user'
+
+class Photo(models.Model):
+    images = models.CharField(max_length=45, blank=True, null=True)
+    # image = models.FileField(upload_to='images/')
+    username = models.CharField(max_length=45, blank=True, null=True)
+    ident = models.AutoField(primary_key=True)
+
+    class Meta:
+        managed = False
+        db_table = 'photo'
+
+
+# image = models.ImageField(upload_to='images/')
+
+
+class Mileage(models.Model):
+    username = models.CharField(primary_key=True, max_length=45)
+    email = models.CharField(max_length=45, blank=True, null=True)
+    mileage = models.IntegerField(blank=True, null=True)
+    state = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'mileage'
+
+
+class MileageContact(models.Model):
+    username = models.CharField(max_length=45,primary_key=True)
+    email = models.CharField(max_length=45, blank=True, null=True)
+    price = models.IntegerField(blank=True, null=True)
+    bank = models.CharField(max_length=45, blank=True, null=True)
+    account_number = models.CharField(max_length=45, blank=True, null=True)
+    phone = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'mileage_contact'
+
+
+class Photo(models.Model):
+    images = models.CharField(max_length=100, blank=True, null=True)
+    username = models.CharField(max_length=45, blank=True, null=True)
+    ident = models.AutoField(primary_key=True)
+
+    class Meta:
+        managed = False
+        db_table = 'photo'
+
+
+class User(models.Model):
+    email = models.CharField(primary_key=True, max_length=20)
+    password = models.CharField(max_length=300, blank=True, null=True)
+    phone = models.IntegerField(blank=True, null=True)
+    regis_date = models.DateField(blank=True, null=True)
+    borrow_code = models.IntegerField(blank=True, null=True)
+    regist_code = models.IntegerField(blank=True, null=True)
+    major = models.CharField(max_length=45, blank=True, null=True)
+    undergrad = models.CharField(max_length=45, blank=True, null=True)
+    username = models.CharField(max_length=45, blank=True, null=True)
+    name = models.CharField(max_length=45, blank=True, null=True)
+    mileage = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'user'
---
+++ proj/urls.py
@@ -0,0 +1,62 @@
+from django.contrib import admin
+from django.urls import path, include
+from app import views as ap
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView
+# from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('all/', ap.db_view),
+    # path('vis/', ap.vis_view),
+    path('bui/', ap.bui_view, name='bui'),
+    path('', ap.main_view, name='main'),
+    path('borrow/', ap.borrow, name='borrow'),
+    path('contact/', ap.contact, name='contact'),
+    path('detail/', ap.detail, name='detail'),
+    path('find_id_pwd/', ap.find_id_pwd, name='find_id_pwd'),
+    path('find_id/<int:studentNum>/<str:name>/', ap.find_id, name='find_id'),
+    path('login/', ap.login, name='login'),
+    path('changepassword/', ap.changepassword, name='changepassword'),
+    path('restrictiondetailsofuse/', ap.restrictiondetailsofuse, name='restrictiondetailsofuse'),
+    # path('login/', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
+    # path('login/', LoginView.as_view(template_name='app/login.html'), name='login'),
+    path('mypage/', ap.mypage, name='mypage'),
+    path('passwdreset/', ap.passwdreset, name='passwdreset'),
+    path('regist/', ap.regist, name='regist'),
+    path('rented_other_detail/', ap.rented_other_detail, name='rented_other_detail'),
+    path('search_result/', ap.search_result, name='search_result'),
+    path('sign_up/', ap.sign_up, name='sign_up'),
+    path('sign_in/', ap.sign_in, name='sign_in'),
+    path('wish_detail/<int:ident>/', ap.wish_detail, name='wish_detail'),
+    path('logout/', ap.logout, name='logout'),
+    path('manage/', ap.manage, name='manage'),
+    path('manage_detail/<int:ident>/', ap.manage_detail, name='manage_detail'),
+    path('borrow_detail/<int:ident>/', ap.borrow_detail, name='borrow_detail'),
+    path('wish/', ap.wish, name='wish'),
+    path('contact/', ap.contact, name='contact'),
+    path('my_contact', ap.my_contact, name='my_contact'),
+    path('what_i_borrowed', ap.what_i_borrowed, name='what_i_borrowed'),
+    path('what_i_borrowed_detail/<int:ident>/', ap.what_i_borrowed_detail, name='what_i_borrowed_detail'),
+    path('', include('app.urls')),
+    path('search/', ap.search, name="search"),
+    path('index/', ap.index, name='index'),
+    path('approval/', ap.approval, name='approval'),
+    path('withdraw/',ap.withdraw, name="withdraw"),
+    # path('/', include('django.contrib.auth.urls')),
+    # +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # path('borrow_detail/', ap.detail, name="borrow_detail"),
+    path('change_persnal',ap.change_persnal, name="change_persnal"),
+    path('my_withdraw/',ap.my_withdraw, name="my_withdraw"),
+    path('edit_myarticle/<int:ident>/', ap.edit_myarticle, name='edit_myarticle'),
+    # path('/', include('django.contrib.auth.urls')),
+    # +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # path('borrow_detail/', ap.detail, name="borrow_detail"),
+
+
+    # path('login/', ap.LoginView.as_view(), name='login'),
+    # path('logout/', ap.LogoutView.as_view(), name='logout'),
+    # path("accounts/",include('accounts.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
